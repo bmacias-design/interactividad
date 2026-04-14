e6c74a932cb21a6dbaeea27ff907f587b21152e6
Corvii <bmacias@centro.edu.mx>
2026-04-14 12:27:15 -0600

    Refactor click event to change background color to random RGB values

--- js/JS script.js
+++ js/JS script.js
@@ -1,12 +1,17 @@
+document.addEventListener("click", function() {
 
+    document.body.style.backgroundColor = "red";
 
-let red=0 , green = 0, blue=0;
+}) 
+let red = 0 , green = 0, blue = 0;
 
 
 document.body.addEventListener('click', changeColor);
  
 function changeColor() {
-    const random = Math.random() * 255;
+    red = Math.random() * 255;
+    green = Math.random() * 255;
+    blue = Math.random() * 255;
     const rgb = "rgb(" + red + "," + green + "," + blue + ")";
     document.body.style.backgroundColor = rgb;
 }
